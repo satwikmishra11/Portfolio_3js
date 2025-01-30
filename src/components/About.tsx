c081651bb7271a787ed746d16d0956ab6a8429ba
satwikmishra11 <150122188+satwikmishra11@users.noreply.github.com>
2025-01-30 18:36:16 +0530

    Uploading Files
    All the Necessary Files Required To  Run The Project

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          I am a full stack developer with expertise in machine learning and
+          holding B.tech in cse , I've  held projects like Zenith OS and
+          Sahsarthi flutter.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
