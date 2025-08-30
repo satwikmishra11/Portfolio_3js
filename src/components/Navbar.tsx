54ea287e2f4eb0ba2cff19345781db0269be0383
Satwik Mishra <150122188+satwikmishra11@users.noreply.github.com>
2025-08-30 10:57:00 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
@@ -25,14 +26,12 @@ const Navbar = () => {
     let links = document.querySelectorAll(".header ul a");
     links.forEach((elem) => {
       let element = elem as HTMLAnchorElement;
-@@ -31,13 +16,13 @@ const Navbar = () => {
+      element.addEventListener("click", (e) => {
+        if (window.innerWidth > 1024) {
           e.preventDefault();
           let elem = e.currentTarget as HTMLAnchorElement;
           let section = elem.getAttribute("data-href");
           smoother.scrollTo(section, true, "top top");
-
-
-
         }
       });
     });
@@ -42,3 +41,41 @@ const Navbar = () => {
   }, []);
   return (
     <>
+      <div className="header">
+        <a href="/#" className="navbar-title" data-cursor="disable">
+          Logo
+        </a>
+        <a
+          href="mailto:satwikmishra46@mail.com"
+          className="navbar-connect"
+          data-cursor="disable"
+        >
+          satwikmishra46@mail.com
+        </a>
+        <ul>
+          <li>
+            <a data-href="#about" href="#about">
+              <HoverLinks text="ABOUT" />
+            </a>
+          </li>
+          <li>
+            <a data-href="#work" href="#work">
+              <HoverLinks text="WORK" />
+            </a>
+          </li>
+          <li>
+            <a data-href="#contact" href="#contact">
+              <HoverLinks text="CONTACT" />
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="landing-circle1"></div>
+      <div className="landing-circle2"></div>
+      <div className="nav-fade"></div>
+    </>
+  );
+};
+
+export default Navbar;
